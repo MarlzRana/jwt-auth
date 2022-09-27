83540c4c38f490355edd236ea3609c89d33af582
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2022-09-27 20:34:01 +0100

    created basic express app

---
+++ server.js
@@ -0,0 +1,8 @@
+// Import modules
+const express = require("express");
+
+// Set up the necessary modules
+const app = express();
+
+// Tell the server to listen to requests on port 3000
+app.listen(3000);
