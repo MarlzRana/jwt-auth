b3846de971440f83b17de475b1649ec465fa2a31
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2022-09-27 20:37:52 +0100

    created boilerplate posts and route to get them

--- server.js
+++ server.js
@@ -4,5 +4,21 @@ const express = require("express");
 // Set up the necessary modules
 const app = express();
 
+// Some boilerplate posts
+const posts = [
+  {
+    username: "marlzrana",
+    title: "learning jwt auth",
+  },
+  {
+    username: "mrrana",
+    title:
+      "jwt auth and it's power over session based auth when using dealing with a microservice architecture",
+  },
+];
+
+// API routes and logic
+app.route("/posts").get((req, res) => res.json(posts));
+
 // Tell the server to listen to requests on port 3000
 app.listen(3000);
